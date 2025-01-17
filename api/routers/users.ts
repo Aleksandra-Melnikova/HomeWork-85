18466b7ac3b3902d521a7cb3287e498f80d76f57
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-17 16:27:38 +0600

    [HW-86 API] Add Auth  and changed logic UserModel and user router.

--- api/routers/users.ts
+++ api/routers/users.ts
@@ -1,10 +1,54 @@
+// import express from 'express';
+// import User from '../models/User';
+// import { Error } from 'mongoose';
+//
+// const usersRouter = express.Router();
+//
+// usersRouter.post('/', async (req, res, next) => {
+//     try {
+//         const user = new User({
+//             username: req.body.username,
+//             password: req.body.password
+//         });
+//         user.generateToken();
+//         await user.save();
+//         res.send(user);
+//     } catch (error) {
+//         if (error instanceof Error.ValidationError) {
+//             res.status(400).send(error);
+//             return
+//         }
+//         next(error);
+//     }
+// });
+//
+// usersRouter.post('/sessions', async (req, res) => {
+//     const user = await User.findOne({username: req.body.username});
+//     if (!user) {
+//         res.status(400).send({error: 'Username not found'});
+//         return
+//     }
+//     const isMatch = await user.checkPassword(req.body.password);
+//     if (!isMatch) {
+//         res.status(400).send({error: 'Password is wrong'});
+//         return
+//     }
+//     user.generateToken();
+//     await user.save();
+//     res.send({message: 'Username and password correct!', user});
+// });
+//
+//
+// export default usersRouter;
 import express from 'express';
 import User from '../models/User';
 import { Error } from 'mongoose';
+import auth from '../middlewear/auth';
+import { RequestWithUser } from '../middlewear/auth';
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', async (req, res, next) => {
+usersRouter.post('/register', async (req, res, next) => {
     try {
         const user = new User({
             username: req.body.username,
@@ -12,7 +56,7 @@ usersRouter.post('/', async (req, res, next) => {
         });
         user.generateToken();
         await user.save();
-        res.send(user);
+        res.send({user, message:"Register successfully"});
     } catch (error) {
         if (error instanceof Error.ValidationError) {
             res.status(400).send(error);
@@ -20,6 +64,7 @@ usersRouter.post('/', async (req, res, next) => {
         }
         next(error);
     }
+
 });
 
 usersRouter.post('/sessions', async (req, res) => {
@@ -38,5 +83,34 @@ usersRouter.post('/sessions', async (req, res) => {
     res.send({message: 'Username and password correct!', user});
 });
 
+usersRouter.delete('/sessions', auth, async (req, res,next) => {
+    let reqWithAuth = req as RequestWithUser
+    const userFromAuth = reqWithAuth.user;
+    try {
+        const user = await User.findOne({_id:userFromAuth._id});
+        if(user){
+            user.generateToken();
+            await user.save();
+        }
+        res.send({
+            message: 'Success logout',
+        });
+
+    }catch (e) {
+        next(e);
+    }
+
+});
+
+usersRouter.post('/secret', auth, async (req, res) => {
+    let expressReq = req as RequestWithUser
+    const user = expressReq.user;
+
+    res.send({
+        message: 'Secret message',
+        user: user
+    });
+
+});
 
 export default usersRouter;
