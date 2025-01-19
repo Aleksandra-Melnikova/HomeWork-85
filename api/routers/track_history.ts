3092035957a62f76b488b7d0b6756b028d56170b
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-19 21:30:02 +0600

    [HW-86 Api] Refactor code

--- api/routers/track_history.ts
+++ api/routers/track_history.ts
@@ -25,12 +25,13 @@ trackHistoryRouter.post('/', async (req, res, next) => {
         const track = await Track.findById(req.body.track);
         if (!track) res.status(404).send({error:'Not Found track'});
         const album = await Album.findById({_id: track?.album});
+        if (!track) res.status(404).send({error:'Not Found album'});
         artist = await Artist.findById({_id: album?.artist});}
 
     const newTrackHistory = {
         user: user._id,
         track: req.body.track,
-        artistName: artist?.name
+        artistName: artist? artist?.name: "anonimus",
     };
 
     try {
